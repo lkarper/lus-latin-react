d47edde53a5eeb68507d9f8f04151833c3ad2bdc
Luther Karper <luther.karper@live.com>
2020-06-12 11:26:23 -0400

    Setup story preview page

--- src/Fabulae/Fabulae.js
+++ src/Fabulae/Fabulae.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import STORE from '../STORE';
 
 class Fabulae extends Component {
     render() {
+        const contentPreview = Object.keys(STORE).map(fab => {
+            const story = STORE[fab];
+            return (
+                <article key={fab}>
+                    <h3><Link>{story.title}</Link></h3> {/*Set up link with router so that the story loads*/}
+                    <p>{`${story.content[0]}...`}</p>
+                </article>
+            )
+        })
         return (
             <main>
                 <h2>Fabulae Latinae</h2>
                 <section>
-                    <h3>Deserunt proident fugiat reprehenderit irure et ea nisi ea.</h3>
-                    <p>Eu incididunt voluptate occaecat do enim nulla nisi ut commodo ex. Irure aliquip est ut id. Amet anim incididunt minim nisi labore qui id tempor consequat exercitation magna laboris ex labore. Velit nisi elit nulla fugiat.</p>
+                    {contentPreview}
                 </section>
             </main>
         );
---
+++ src/STORE.js
@@ -0,0 +1,28 @@
+const STORE = {
+    "1": {
+        "title": "Spīnōsa ēricia amīcum vult.",
+        "content": [
+            "Spīnōsa ēricia est.",
+            "Spīnōsa ēricia fēminīna est.",
+            "Est ēricia, cui nōmen Spīnōsa est.",
+            "Nōmen huic ēriciae est Spīnōsa, quod ea spīnās habet.",
+            "Quae sunt spinae?",
+            "Spīnōsa spīnās habet, ergō nōmen eī Spīnōsa est.",
+            "Spīnōsa in arbore habitat.",
+            "Spīnōsa inter rāmōs arboris nōn habitat, sed inter rādicēs.",
+            "Avēs inter rāmōs arboris habitant, sed nōn Spīnōsa.",
+            "Ubi Spīnōsa habitat?",
+            "Spīnōsa inter rādicēs arboris habitat.",
+            "Placet Spīnōsae in arbore habitāre.",
+            "Placet eī quoque in silvā ambulāre.",
+            "Spīnōsa autem nōn cum amīcō sed sōla ambulat.",
+            "Spīnōsa nūllum amīcum habet, ergō ea sōla ambulat.",
+            "Displicet eī nūllum amīcum habēre.",
+            "Spīnōsa nōn laeta est et amīcum vult.",
+            "\"Volō cum amīcō in silvā ambulāre!\", dīcit Spīnōsa.",
+            "Spīnōsa amīcum vult, ergō amīcum quaerit."
+        ]
+    }
+}
+
+export default STORE;
