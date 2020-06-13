4d7cb353095e9ab8d352ac83cd73ab35d2b14d82
Luther Karper <luther.karper@live.com>
2020-06-13 14:58:37 -0400

    Add api calls in Fabula and Fabulae components

--- src/Fabula/Fabula.js
+++ src/Fabula/Fabula.js
@@ -1,59 +1,80 @@
 import React, { Component } from 'react';
 import WordsList from '../WordsList/WordsList';
-import STORE from '../STORE';
+import ENV from '../config';
 import './Fabula.css';
 
 class Fabula extends Component {
 
-    state ={
+    state = {
+        story: {},
         currentWord: {},
     }
 
     setCurrentWord = (e, place) => {
         e.preventDefault();
-        const {genre, id } = this.props.match.params;
-        const story = STORE[genre][id];
+        const { story } = this.state;
         const currentWord = story.vocab[place];
         this.setState({
             currentWord: { [currentWord.word]: currentWord.def },
         });
     }
 
-    render() {
-        const {genre, id } = this.props.match.params;
-        const story = STORE[genre][id];
-        const storyLines = story.content.map((line, index) => {
-            const wordButtons = line
-                .split(' ')
-                .map((word, i) => {
-                    if (Object.keys(story.vocab).includes(`line:${index + 1}_word:${i + 1}`)) {  
-                        return (
-                            <button 
-                                key={i} 
-                                onClick={(e)=> this.setCurrentWord(e, `line:${index + 1}_word:${i + 1}`)}
-                            >
-                                {word}
-                            </button>
-                        );
-                    }
-                    return <span key={i}>{` ${word} `}</span>;
-                }); 
-            return (
-                <li key={index}>
-                    <p>{wordButtons}</p>
-                </li>);
+    componentDidMount() {
+        const { genre, id } = this.props.match.params;
+        const url = `http://localhost:8000/fabulae?genre=${genre}&id=${id}`;
+        const options = { 
+            headers: new Headers({
+                "Authorization": `Bearer ${ENV.API_TOKEN || '6f06a6cd-90ea-4e28-ab97-9e02ba042203'}`,
+            }),
+        };
+        fetch(url, options)
+            .then(res => res.json())
+            .then(data => {
+                this.setState({
+                    story: data,
+                });
+                console.log(data);
             });
-        return (
-            <>
-                <article className="Fabula__container">
-                    <h3>{story.title}</h3>
-                    <ol>
-                        {storyLines}
-                    </ol>
-                </article>
-                <WordsList currentWord={this.state.currentWord}/>
-            </>
-        );
+    }
+
+    render() {
+        const { story } = this.state;
+        if (Object.keys(story).length) {
+            const storyLines = story.content.map((line, index) => {
+                const wordButtons = line
+                    .split(' ')
+                    .map((word, i) => {
+                        if (Object.keys(story.vocab).includes(`line:${index + 1}_word:${i + 1}`)) {  
+                            return (
+                                <button 
+                                    key={i} 
+                                    onClick={(e)=> this.setCurrentWord(e, `line:${index + 1}_word:${i + 1}`)}
+                                >
+                                    {word}
+                                </button>
+                            );
+                        }
+                        return <span key={i}>{` ${word} `}</span>;
+                    }); 
+                return (
+                    <li key={index}>
+                        <p>{wordButtons}</p>
+                    </li>);
+                });
+            return (
+                <>
+                    <article className="Fabula__container">
+                        <h3>{story.title}</h3>
+                        <ol>
+                            {storyLines}
+                        </ol>
+                    </article>
+                    <WordsList currentWord={this.state.currentWord}/>
+                </>
+            );
+        } else {
+            return <p className="Fabula__loading">Loading...</p>;
+        }
     }
 }
 
--- src/FabulaeList/FabulaeList.js
+++ src/FabulaeList/FabulaeList.js
@@ -1,28 +1,68 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import STORE from '../STORE';
+import ENV from '../config';
 
 class Fabulae extends Component {
+
+    state = {
+        fabulae: [],
+    }
+
+    fetchFabulae = (genre) => {
+        const url = `http://localhost:8000/fabulae?genre=${genre}`;
+        const options = { 
+            headers: new Headers({
+                "Authorization": `Bearer ${ENV.API_TOKEN || '6f06a6cd-90ea-4e28-ab97-9e02ba042203'}`,
+            }),
+        };
+        console.log(url, options);
+        fetch(url, options)
+            .then(res => res.json())
+            .then(data => {
+                this.setState({
+                    fabulae: data,
+                });
+                console.log(data);
+            });
+    }
+
+    componentDidMount() {
+        const { genre } = this.props.match.params;
+        this.fetchFabulae(genre);
+    }
+
+    componentWillReceiveProps(nextProps) {
+        const { genre } = nextProps.match.params;
+        this.fetchFabulae(genre);
+    }
+
     render() {
         const genre = this.props.match.params.genre;
-        const stories = STORE[genre];
-        const contentPreview = Object.keys(stories).map(id => {
-            const story = stories[id];
-            return (
-                <li key={id}>
-                    <h3>
-                        <Link 
-                            to={`/fabulae/${genre}/${id}`}        
-                        >
-                            {story.title}
-                        </Link></h3> 
-                    <p>{`${story.content[0]}...`}</p>
-                </li>
-            )
-        });
+        let contentPreview;
+
+        if (this.state.fabulae.length) {
+            contentPreview = this.state.fabulae.map(fab => {
+                const id = Object.keys(fab)[0];
+                const title = fab[id].title;
+                const preview = fab[id].preview;
+                return (
+                    <li key={id}>
+                        <h3>
+                            <Link 
+                                to={`/fabulae/${genre}/${id}`}        
+                            >
+                                {title}
+                            </Link></h3> 
+                        <p>{preview}</p>
+                    </li>
+                )
+            });
+        } else {
+            contentPreview = <p className="FabulaeList__loading">Loading...</p>
+        }
         return (
             <main>
-                <h2>Fabulae Latinae</h2>
+                <h2>{`Fabulae ${genre.replace(genre.charAt(0), genre.charAt(0).toUpperCase())}`}</h2>
                 <ul>
                     {contentPreview}
                 </ul>
