52b1e34f594e89b5c13546f1dc6b33c1090c3915
Luther Karper <luther.karper@live.com>
2020-06-18 10:04:35 -0400

    Add snapshot test to Dictionary component

--- src/Dictionary/Dictionary.test.js
+++ src/Dictionary/Dictionary.test.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { shallow } from 'enzyme';
+import toJSON from 'enzyme-to-json';
 import Dictionary from './Dictionary';
 
 describe('Dictionary component', () => {
     it('renders without crashing', () => {
         const div = document.createElement('div');
-        ReactDOM.render(<BrowserRouter><Dictionary /></BrowserRouter>, div);
+        ReactDOM.render(
+            <BrowserRouter>
+                <Dictionary />
+            </BrowserRouter>, 
+            div
+        );
         ReactDOM.unmountComponentAtNode(div);
+    });
+
+    it('renders the UI as expected', () => {
+        const wrapper = shallow(
+            <BrowserRouter>
+                <Dictionary />
+            </BrowserRouter>
+        );
+        expect(toJSON(wrapper)).toMatchSnapshot();
     });
 });
